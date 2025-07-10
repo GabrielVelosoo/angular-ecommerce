d068a24aea372fe06c5defcf98bda0ae28ba62de
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-07-10 14:37:23 -0300

    feat(menu): refatoramento para as opções do menu virem do backend de forma dinâmica

---
+++ src/app/features/home/services/categoria.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Categoria } from '../../../models/Categoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  apiUrl: string = environment.apiBaseUrl + '/api/categorias'
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  obterCategorias(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(this.apiUrl);
+  }
+}
--- src/app/layout/components/menu/menu.component.ts
+++ src/app/layout/components/menu/menu.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../features/auth/services/auth.service';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { Router } from '@angular/router';
+import { CategoriaService } from '../../../features/home/services/categoria.service';
+import { Categoria } from '../../../models/Categoria';
 
 @Component({
   standalone: false,
@@ -9,13 +11,29 @@ import { Router } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  categorias: Categoria[] = [];
 
   constructor(
     private router: Router,
     private authService: AuthService,
     private oauthService: OAuthService,
+    private categoriaService: CategoriaService
   ) { }
+
+  ngOnInit(): void {
+    this.categoriaService
+      .obterCategorias()
+      .subscribe({
+        next: (response) => {
+          this.categorias = response;
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      })
+  }
 
   navegarUsuario() {
     if(this.oauthService.hasValidAccessToken()) {
